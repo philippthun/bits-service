3152ed2809a08d45ed74b436e81f2960ef70dd90
Tobias Zipfel <tobias.zipfel@de.ibm.com>
2018-09-14 11:51:39 +0200

    Adjust test setup
    [#160228241]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- package_bundle_test.go
+++ package_bundle_test.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 
 	bitsgo "github.com/cloudfoundry-incubator/bits-service"
 	inmemory "github.com/cloudfoundry-incubator/bits-service/blobstores/inmemory"
@@ -40,26 +41,62 @@ var _ = Describe("CreateTempZipFileFrom", func() {
 		Expect(reader.File).To(HaveLen(1))
 		VerifyZipFileEntry(&reader.Reader, "filename1", "filename1 content")
 	})
+	Context("Handles the 'Modified time' property", func() {
+		It("should not contain '1979-11-30'", func() {
+			Expect(blobstore.Put("abc", strings.NewReader("filename1 content"))).To(Succeed())
 
-	It("Expects a zip with unchanged 'Modified time' property", func() {
-		Expect(blobstore.Put("abc", strings.NewReader("filename1 content"))).To(Succeed())
+			tempFileName, e := bitsgo.CreateTempZipFileFrom([]bitsgo.Fingerprint{
+				bitsgo.Fingerprint{
+					Sha1: "abc",
+					Fn:   "filename1",
+					Mode: "644",
+				},
+			}, nil, 0, math.MaxUint64, blobstore, NewMockMetricsService())
+			Expect(e).NotTo(HaveOccurred())
 
-		tempFileName, e := bitsgo.CreateTempZipFileFrom([]bitsgo.Fingerprint{
-			bitsgo.Fingerprint{
-				Sha1: "abc",
-				Fn:   "filename1",
-				Mode: "644",
-			},
-		}, nil, 0, math.MaxUint64, blobstore, NewMockMetricsService())
-		Expect(e).NotTo(HaveOccurred())
+			reader, e := zip.OpenReader(tempFileName)
+			Expect(e).NotTo(HaveOccurred())
+			Expect(reader.File).To(HaveLen(1))
+			lm := reader.File[0].FileHeader.Modified
+			lastmodified := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n", lm.Year(), lm.Month(), lm.Day(),
+				lm.Hour(), lm.Minute(), lm.Second())
+			Expect(string(lastmodified)).NotTo(ContainSubstring("1979-11-30"))
+		})
+		It("should not be manipulated for uploaded files", func() {
+			tmpfile, modTime := createTmpFile()
+			tmpfilereader, e := os.Open(tmpfile)
+			defer os.Remove(tmpfile)
+			Expect(e).NotTo(HaveOccurred())
+			response := blobstore.Put("abc", tmpfilereader)
+			fmt.Printf("%v", response)
+			Expect(response).To(Succeed())
 
-		reader, e := zip.OpenReader(tempFileName)
-		Expect(e).NotTo(HaveOccurred())
-		Expect(reader.File).To(HaveLen(1))
-		lm := reader.File[0].FileHeader.Modified
-		lastmodified := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n", lm.Year(), lm.Month(), lm.Day(),
-			lm.Hour(), lm.Minute(), lm.Second())
-		Expect(string(lastmodified)).NotTo(ContainSubstring("1979-11-30"))
+			tempFileName, e := bitsgo.CreateTempZipFileFrom([]bitsgo.Fingerprint{
+				bitsgo.Fingerprint{
+					Sha1: "abc",
+					Fn:   "filename1",
+					Mode: "644",
+				},
+			}, nil, 0, math.MaxUint64, blobstore, NewMockMetricsService())
+			Expect(e).NotTo(HaveOccurred())
+			reader, e := zip.OpenReader(tempFileName)
+			Expect(e).NotTo(HaveOccurred())
+			lm := reader.File[0].FileHeader.Modified
+			Expect(lm).To(Equal(modTime))
+		})
+		It("should provide the current datetime for files retrieved from the bundles_cache", func() {
+			Expect(blobstore.Put("abc", strings.NewReader("filename1 content"))).To(Succeed())
+
+			_, e := bitsgo.CreateTempZipFileFrom([]bitsgo.Fingerprint{
+				bitsgo.Fingerprint{
+					Sha1: "abc",
+					Fn:   "filename1",
+					Mode: "644",
+				},
+			}, nil, 0, math.MaxUint64, blobstore, NewMockMetricsService())
+			Expect(e).NotTo(HaveOccurred())
+		})
+
 	})
 
 	Context("One error from blobstore", func() {
@@ -162,3 +199,18 @@ var _ = Describe("CreateTempZipFileFrom", func() {
 		})
 	})
 })
+
+func createTmpFile() (string, time.Time) {
+	content := []byte("filename1 content")
+	tmpfile, e := ioutil.TempFile("/tmp/unit-test", "example")
+	Expect(e).NotTo(HaveOccurred())
+
+	_, e = tmpfile.Write(content)
+	Expect(e).NotTo(HaveOccurred())
+	fileInfo, e := tmpfile.Stat()
+	Expect(e).NotTo(HaveOccurred())
+	e = tmpfile.Close()
+	Expect(e).NotTo(HaveOccurred())
+	modTime := fileInfo.ModTime()
+	return tmpfile.Name(), modTime
+}
