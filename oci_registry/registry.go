3a1bf1d79804f826087bce95fd1a8e9cc31ee34e
Tobias Zipfel <tobias.zipfel@de.ibm.com>
2018-09-18 16:14:33 +0200

    Clean-up
    [#156599754]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- oci_registry/registry.go
+++ oci_registry/registry.go
@@ -27,16 +27,6 @@ type APIVersionHandler struct {
 	apiVersionManager APIVersionManager
 }
 
-// func NewHandler(imageManager ImageManager, apiVersionManager APIVersionManager) http.Handler {
-// 	mux := mux.NewRouter()
-// 	imageHandler := ImageHandler{imageManager}
-// 	apiVersionHandler := APIVersionHandler{apiVersionManager}
-// 	mux.Path("/v2/").Methods(http.MethodGet).HandlerFunc(apiVersionHandler.APIVersion)
-// 	mux.Path("/v2/{name:[a-z0-9/\\.\\-_]+}/manifest/{tag}").Methods(http.MethodGet).HandlerFunc(imageHandler.ServeManifest)
-// 	mux.Path("/v2/{name:[a-z0-9/\\.\\-_]+}/blobs/{digest}").Methods(http.MethodGet).HandlerFunc(imageHandler.ServeLayer)
-// 	return mux
-// }
-
 func NewImageHandler(imageManager ImageManager) http.Handler {
 	mux := mux.NewRouter()
 	imageHandler := ImageHandler{imageManager}
